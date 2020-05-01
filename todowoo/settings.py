0bb05d16ce4427aaf7adc97c03bda41eb8ef246b
darshansoni09 <darshansoni865@gmail.com>
2020-05-01 21:22:36 +0530

    New Local Settings

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -123,3 +123,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 LOGIN_URL = '/login'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Looks like no local files. You must be on production")
